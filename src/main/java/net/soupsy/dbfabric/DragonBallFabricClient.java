d683210ad34315c7d7363b75f50764ef9b69b8c5
SoupsyLive <subtomyytplzz@gmail.com>
2023-04-27 15:27:53 -0700

    initial

---
+++ src/main/java/net/soupsy/dbfabric/DragonBallFabricClient.java
@@ -0,0 +1,10 @@
+package net.soupsy.dbfabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DragonBallFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
