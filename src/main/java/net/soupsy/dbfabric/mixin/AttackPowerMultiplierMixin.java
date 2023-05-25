08f936bb784f3c802af0a987976aa41e8f42a3c6
SoupsyLive <subtomyytplzz@gmail.com>
2023-05-24 22:42:45 -0700

    Working better, almost finished with the attack multiplier, :)
    - AttackPowerMultiplierMixin addition of multiplyDamage

--- src/main/java/net/soupsy/dbfabric/mixin/AttackPowerMultiplierMixin.java
+++ src/main/java/net/soupsy/dbfabric/mixin/AttackPowerMultiplierMixin.java
@@ -2,32 +2,40 @@ package net.soupsy.dbfabric.mixin;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import net.soupsy.dbfabric.util.IEntityDataSaver;
 import net.soupsy.dbfabric.util.PowerData;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(PlayerEntity.class)
 public abstract class AttackPowerMultiplierMixin extends Entity {
 
+    @Shadow public abstract float getAttackCooldownProgress(float baseTime);
+
     public AttackPowerMultiplierMixin(EntityType<?> type, World world) {
         super(type, world);
     }
 
-    @ModifyVariable(method = "attack", at = @At("HEAD"), argsOnly = false)
-    public PlayerEntity multiplyDamage(PlayerEntity target) {
+    @ModifyVariable(method = "attack", at = @At("STORE"), ordinal = 0)
+    protected float multiplyDamage(float f) {
 
         // get NBT data of person
         IEntityDataSaver dataPlayer = (IEntityDataSaver) this;
-        int power = PowerData.getPower(dataPlayer);
+        float power = PowerData.getPower(dataPlayer);
 
-        // apply modifier to variable f, still working on that.
+        // create a temp F to use in the return
+        float tempF = (float) this.getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE);
 
-        return target;
+        //this.getAttackCooldownProgress();
+
+        // return the same F in the original multiplies by my modifier
+        return tempF * (power/20);
     }
 
 
